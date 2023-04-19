ea0543cdb2187ce59635903052819eb68d3ee648
Cristian Amaya <cristiancisse12@protonmail.com>
2023-04-19 18:33:22 -0500

    incluida carpeta php mas la conexion a la base de datos del formulario del index.html

---
+++ php/con_db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$port = "3307";
+$username = "root";
+$password = "123456";
+$dbname = "landing";
+
+$conex = new mysqli($servername, $username, $password, $dbname, $port);
+?>
---
+++ php/enviar_mensaje.php
@@ -0,0 +1,30 @@
+<?php
+include("/php/con_db.php");
+
+if (isset ($_POST['enviar'])) {
+    if (strlen($_POST['email'])  >= 1 && strlen($_POST['mensaje']) >= 1) {
+        $email = trim($_POST['email']);
+        $mensaje = trim($_POST['mensaje']);
+        $mens = "INSERT INTO datos(email,mensaje) VALUES ('$email','$mensaje')";
+        $resultado = mysqli_query($conex,$mens);
+        
+        if ($resultado){
+
+                ?>
+                <h3>Mensaje enviado.</h3>
+                <?php
+
+        } else {
+            ?>
+            <h3> Ha ocurrido un error.</h3>
+            <?php
+        
+        } 
+    }   else {
+        ?>
+        <h3> Complete todos los campos.</h3>
+        <?php
+    }    
+}
+
+?>
